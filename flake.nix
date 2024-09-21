ac98ffc31ee2d8e8cd7e987186798f8ff8814bec
dosed-dev <dev.dozed@aleeas.com>
2024-09-22 00:55:28 +0300

    Remove packwiz2nix

--- flake.nix
+++ flake.nix
@@ -4,42 +4,16 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    packwiz2nix = {
-      url = "github:dozed-dev/packwiz2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = {
     nixpkgs,
     flake-utils,
-    packwiz2nix,
-    self,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {inherit system;};
-      inherit
-        (packwiz2nix.lib.${system})
-        fetchPackwizModpack
-        mkMultiMCPack
-        ;
     in {
-      packages = {
-        modpack = fetchPackwizModpack {
-          manifest = "${self}/packwiz/pack.toml";
-          hash = "sha256-7M38w7N5sdYkSfBh14M9GhZSEWZdOyTe0+Blf3hpigk=";
-        };
-        modpack-zip = mkMultiMCPack {
-          src = self.packages.${system}.modpack;
-          instanceCfg = ./files/instance.cfg;
-          extraFiles = {
-            "mmc-pack.json" = ./files/mmc-pack.json;
-          };
-        };
-        default = self.packages.${system}.modpack-zip;
-      };
-
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
           packwiz
