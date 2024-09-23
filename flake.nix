a26a72ae4afea51e5be97dee109e4700226fbba9
dosed-dev <dev.dozed@aleeas.com>
2024-09-24 01:49:46 +0300

    Add authlib-injector again

--- flake.nix
+++ flake.nix
@@ -22,7 +22,12 @@
       in {
         imports = [ nix-minecraft.nixosModules.minecraft-servers ];
 
-        services.minecraft-servers = {
+        services.minecraft-servers =
+          let authlib-injector = pkgs.fetchurl {
+            url = "https://github.com/yushijinhun/authlib-injector/releases/download/v1.2.5/authlib-injector-1.2.5.jar";
+            hash = "sha256-O8nr3Fg7NqvSpltibEufNfIRd/v0KoUWBuquo/1C7g8=";
+          };
+        in {
           enable = true;
           eula = true;
           servers = {
@@ -33,6 +38,7 @@
               serverProperties = {
                 serverPort = 25565;
               };
+              jvmOpts = "-javaagent:${authlib-injector}=ely.by";
             };
           };
         };
