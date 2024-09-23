1afbceee1412184bcb8f2d6854668ea1da1177f8
dosed-dev <dev.dozed@aleeas.com>
2024-09-24 01:55:38 +0300

    Move nix-minecraft module out

--- flake.nix
+++ flake.nix
@@ -19,30 +19,7 @@
           system = "x86_64-linux";
           config.allowUnfree = true;
         };
-      in {
-        imports = [ nix-minecraft.nixosModules.minecraft-servers ];
-
-        services.minecraft-servers =
-          let authlib-injector = pkgs.fetchurl {
-            url = "https://github.com/yushijinhun/authlib-injector/releases/download/v1.2.5/authlib-injector-1.2.5.jar";
-            hash = "sha256-O8nr3Fg7NqvSpltibEufNfIRd/v0KoUWBuquo/1C7g8=";
-          };
-        in {
-          enable = true;
-          eula = true;
-          servers = {
-            test-server1 = {
-              enable = true;
-              package = pkgs.vanillaServers.vanilla-1_21;
-
-              serverProperties = {
-                serverPort = 25565;
-              };
-              jvmOpts = "-javaagent:${authlib-injector}=ely.by";
-            };
-          };
-        };
-      };
+      in import ./server.nix {inherit nix-minecraft pkgs;};
     };
   };
 }
--- server.nix
+++ server.nix
@@ -1,18 +1,14 @@
-{ inputs, pkgs, ... }: {
-  imports = [ inputs.nix-minecraft.nixosModules.minecraft-servers ];
-  nixpkgs.overlays = [ inputs.nix-minecraft.overlay ];
+{ nix-minecraft, pkgs, ... }: {
+  imports = [ nix-minecraft.nixosModules.minecraft-servers ];
 
   services.minecraft-servers =
-  let
-    authlib-injector = pkgs.fetchurl {
+    let authlib-injector = pkgs.fetchurl {
       url = "https://github.com/yushijinhun/authlib-injector/releases/download/v1.2.5/authlib-injector-1.2.5.jar";
       hash = "sha256-O8nr3Fg7NqvSpltibEufNfIRd/v0KoUWBuquo/1C7g8=";
     };
   in {
     enable = true;
     eula = true;
-    openFirewall = true;
-
     servers = {
       test-server1 = {
         enable = true;
