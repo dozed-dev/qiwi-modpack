6bd1f55a610bfe4ef8b2f48e4286e5218446f22b
dosed-dev <dev.dozed@aleeas.com>
2024-09-24 19:52:07 +0300

    Move capes and debugify from yosbr

--- server.nix
+++ server.nix
@@ -8,7 +8,7 @@
     };
     modpack = pkgs.fetchPackwizModpack {
       url = "https://raw.githubusercontent.com/dozed-dev/qiwi-modpack/main/packwiz/pack.toml";
-      packHash = "sha256-kNZVc6uYQdSYxUFPSM4a+YMPulwZPf7JMDjGt6V+rNs=";
+      packHash = "sha256-56O2JT77Ayf8OCsED52y6IKRZ+0zAE8Qc4K/B3GS/8I=";
     };
     mcVersion = modpack.manifest.versions.minecraft;
     fabricVersion = modpack.manifest.versions.fabric;
